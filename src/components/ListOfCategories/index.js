8a3bef7bb17cde6769c78c7bb467c78886fe01ce
kath_code <kathmene27@gmail.com>
2019-09-20 14:02:19 -0500

    Adding new hook for the categories

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,19 +1,33 @@
 import React, { useState, useEffect } from 'react';
+import { FaSpinner } from 'react-icons/fa';
+
 import { Category } from '../Category';
+import { Spinner } from '../../sharedComponents/Spinner';
 
 import { List, Item } from './styles';
 
-export const ListOfCategories = () => {
+function useCategoriesData() {
   const [categories, setCategories] = useState([]);
-  const [showFixed, setShowFixed] = useState(false);
+  const [isLoading, setIsLoading] = useState(null);
 
   useEffect(() => {
+    setIsLoading(true);
     fetch('https://kath-server-1ihgc93wc.now.sh/categories')
       .then((res) => res.json())
       .then((response) => {
         setCategories(response);
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
   }, []); // [] With it will render once
+
+  return { categories, isLoading };
+}
+
+export const ListOfCategories = () => {
+  const [showFixed, setShowFixed] = useState(false);
+  const { categories, isLoading } = useCategoriesData();
 
   useEffect(() => {
     const onScroll = (e) => {
@@ -22,15 +36,21 @@ export const ListOfCategories = () => {
     };
 
     document.addEventListener('scroll', onScroll);
-  });
+
+    return () => document.removeEventListener('scroll', onScroll);
+  }, [showFixed]);
 
   const renderList = (fixed) => (
-    <List className={fixed ? 'fixed' : ''}>
-      {categories.map((category) => (
-        <Item key={category.id}>
-          <Category {...category} />
-        </Item>
-      ))}
+    <List fixed={fixed}>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        categories.map((category) => (
+          <Item key={category.id}>
+            <Category {...category} />
+          </Item>
+        ))
+      )}
     </List>
   );
 
