b571154e7f5704b081a371c69de3bef50e05216c
kath_code <kathmene27@gmail.com>
2019-09-20 09:24:15 -0500

    Showing the menu when scrolling

--- src/components/PhotoCard/styles.js
+++ src/components/PhotoCard/styles.js
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 import { fadeIn } from '../../styles/animation';
 
+export const ArticleWrapper = styled.article`
+  margin: 20px;
+`;
+
 export const ImgWrapper = styled.div`
   border-radius: 10px;
   display: block;
