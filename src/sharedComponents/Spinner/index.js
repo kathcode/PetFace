c4572c88036dd3d75ec50c9c3e71fe05580f93aa
kath_code <kathmene27@gmail.com>
2019-09-20 14:01:39 -0500

    Creating a spinner component

---
+++ src/sharedComponents/Spinner/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+import { IconSpinner } from './styled';
+
+export const Spinner = () => <IconSpinner />;
---
+++ src/sharedComponents/Spinner/styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { FaSpinner } from 'react-icons/fa';
+
+export const IconSpinner = styled(FaSpinner)`
+    font-size: 2rem;
+    margin: 0 auto;
+    color: #a9ddef;
+`;
