2dbd7b9a1aecdd0302342b7e263d3a2fbe6a016c
kath_code <kathmene27@gmail.com>
2019-09-26 09:00:06 -0500

    Get the photos with GraphQL

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import { graphql } from 'react-apollo';
+import { gql } from 'apollo-boost';
+
 import { PhotoCard } from '../PhotoCard';
 
-export const ListOfPhotoCards = () => (
-  <ul>
-    {[1, 2, 3, 4, 5].map((id) => (
-      <PhotoCard key={id} likeId={id} />
-    ))}
-  </ul>
-);
+const withPhotos = graphql(gql`
+  query getPhotos {
+    photos {
+      id
+      categoryId,
+      src
+      likes
+      userId
+      liked
+    }
+  }
+`); /*
+    Higher order component:
+    A function that receive a component and return a
+    component with injected improvements
+    */
+
+const ListOfPhotoCardsComponent = (props) => {
+  const { data } = props;
+  return (
+    <>
+      {!data.loading && (
+        <ul>
+          {data.photos && data.photos.map((photo) => (
+            <PhotoCard key={photo.id} likeId={photo.id} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
+};
+
+export const ListOfPhotoCards = withPhotos(ListOfPhotoCardsComponent);
