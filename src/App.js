1a5597e861ac081b2c0ee38d73d7e6200ab5bb61
kath_code <kathmene27@gmail.com>
2019-10-11 09:11:31 -0500

    Add the navbar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { GlobalStyle } from './styles/GlobalStyles';
 import { Logo } from './components/Logo';
 import { Home } from './pages/Home';
 import { Detail } from './pages/Detail';
+import { NavBar } from './components/NavBar';
 
 export const App = () => {
   return (
@@ -16,6 +17,7 @@ export const App = () => {
         <Home path="/pet/:id" />
         <Detail path="/detail/:detailId" />
       </Router>
+      <NavBar />
     </div>
   );
 };
