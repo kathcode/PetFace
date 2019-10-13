193a834a5217da32e8335ef21919eb245027f933
Kath_code <kathmene27@gmail.com>
2019-10-12 21:21:20 -0500

    Add page when the user is not register

--- src/App.js
+++ src/App.js
@@ -5,9 +5,15 @@ import { GlobalStyle } from './styles/GlobalStyles';
 import { Logo } from './components/Logo';
 import { Home } from './pages/Home';
 import { Detail } from './pages/Detail';
+import { User } from './pages/User';
+import { Favs } from './pages/Favs';
+import { NoRegisteredUser } from './pages/NoRegisteredUser';
 import { NavBar } from './components/NavBar';
 
 export const App = () => {
+  const UserLogged = ({ children }) => {
+    return children({ isAuth: false });
+  };
   return (
     <div>
       <GlobalStyle />
@@ -17,6 +23,21 @@ export const App = () => {
         <Home path="/pet/:id" />
         <Detail path="/detail/:detailId" />
       </Router>
+      <UserLogged>
+        {({ isAuth }) => {
+          return isAuth ? (
+            <Router>
+              <Favs path="/favs" />
+              <User path="/user" />
+            </Router>
+          ) : (
+            <Router>
+              <NoRegisteredUser path="/favs" />
+              <NoRegisteredUser path="/user" />
+            </Router>
+          );
+        }}
+      </UserLogged>
       <NavBar />
     </div>
   );
